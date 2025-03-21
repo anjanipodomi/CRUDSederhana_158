3e9e63eb93fa066f10e45e7d1847d74ac4036321
anjanipodomi <anjanidihapsari@gmail.com>
2025-03-21 14:31:01 +0700

    namespace CRUDSederhana

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -11,3 +11,37 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
+namespace CRUDSederhana
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source= SARI\\ANJANIPODOMI;Initial Catalog = OrganisasiMahasiswa; Integrated Security = True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        namespace CRUDSederhana
+    {
+        public partial class Form1 : Form
+        {
+            private string connectionString = "Data Source= SARI\\ANJANIPODOMI;Initial Catalog = OrganisasiMahasiswa; Integrated Security = True";
+            public Form1()
+            {
+                InitializeComponent();
+            }
+            private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void label3_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+
+            }
+
